d8eb69d6c0d0f50193b8b421330b7f8e2b0767ab
gluke0 <milanesigianluca@gmail.com>
2023-07-14 14:57:56 +0200

    add dynamic title n real time data n hour in dash

--- resources/views/admin/teachers/dashboard.blade.php
+++ resources/views/admin/teachers/dashboard.blade.php
@@ -1,15 +1,21 @@
 @extends('layouts.app')
 
-@section('title', 'Dashboard')
+@section('title', 'Dashboard | ' . $user)
 
 @section('content')
-<div class="container">
+<div class="container d-flex justify-content-between align-items-center">
     <h2 class="fs-4 text-secondary my-4">
         {{ __('Dashboard') }}
     </h2>
-    <div class="row justify-content-center">
-        <div class="col">
-            {{-- <div class="card">
+
+    <div>
+        <span class="small" id="date">  </span>
+        <span style="font-size: 0.7rem" id="time">  </span>
+    </div>
+</div>
+<div class="container">
+        {{-- <div class="col">
+            <div class="card">
                 <div class="card-header">{{ __('User Dashboard') }}</div>
 
                 <div class="card-body">
@@ -21,10 +27,24 @@
 
                     {{ __('You are logged in!') }}
                 </div>
-            </div> --}}
-        </div>
-        <h2 class="fs-1"> Bentornato {{ $user }}! </h2>
-        <h2>Crea il tuo profilo alla <a class="text-decoration-none" {{route('teacher.create')}}> pagina di registrazione! </a></h2>
-    </div>
+            </div>
+        </div> --}}
+    <h2 class="fs-1"> Bentornato {{ $user }}! </h2>
+    <h2>Crea il tuo profilo alla <a class="text-decoration-none" {{route('teacher.create')}}> pagina di registrazione! </a></h2>
 </div>
+
 @endsection
+
+<script>
+    // real time date and time
+    const zeroFill = n => {
+        return ('0' + n).slice(-2);
+    }
+    const interval = setInterval(() => {
+        const now = new Date();
+        const date = zeroFill(now.getUTCDate()) + ' / ' + zeroFill((now.getMonth() + 1)) + ' / ' + String(now.getFullYear()).slice(-2);
+        const time = zeroFill(now.getHours()) + ':' + zeroFill(now.getMinutes());
+        document.getElementById('date').innerHTML = date;
+        document.getElementById('time').innerHTML = time;
+    }, 1000);
+</script>
