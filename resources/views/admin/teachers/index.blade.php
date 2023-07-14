bad1563f92c83674ed3d95e12ff5569105f05a9f
Cicidong00 <136182931+Cicidong00@users.noreply.github.com>
2023-07-14 11:39:43 +0200

    Aggiunta possibilità di aprire file pdf

--- resources/views/admin/teachers/index.blade.php
+++ resources/views/admin/teachers/index.blade.php
@@ -8,9 +8,10 @@
 
     
     <div class="card">
-      <img src="{{asset('storage/' . $teacher->profile_picture)}}" class="card-img-top" alt="" width="90%">
+      {{-- <img src="{{asset('storage/' . $teacher->profile_picture)}}" class="card-img-top" alt="" width="90%"> --}}
       <div class="card-body">
         <h5 class="card-title">{{ $user->name }}</h5>
+        <a href="{{asset('storage/' . $teacher['profile_picture'])}}">Guarda il CV</a>
         <p class="card-text">{{$teacher->description}}</p>
         <p class="card-text">{{$teacher->price}}</p>
         <a href="{{route('teacher.edit', $teacher->id)}}" class="btn btn-success">Modifica il profilo</a>
