209be3d5e35f850c4b8b7c7c1d3918254d0dbc09
gluke0 <milanesigianluca@gmail.com>
2023-07-17 14:35:26 +0200

    more translations

--- lang/en/passwords.php
+++ lang/en/passwords.php
@@ -13,10 +13,17 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    // 'reset' => 'Your password has been reset!',
+    // 'sent' => 'We have emailed your password reset link!',
+    // 'throttled' => 'Please wait before retrying.',
+    // 'token' => 'This password reset token is invalid.',
+    // 'user' => "We can't find a user with that email address.",
+
+    'reset' => 'La tua password è stata cambiata',
+    'sent' => 'Ti abbiamo inviato il link per cambiare la tua password!',
+    'throttled' => 'Per favore, aspetta prima di riprovare.',
+    'token' => 'Il token per il reset della password non è valido.',
+    'user' => "Non abbiamo trovato nessun user con questa email.",
+
 
 ];
