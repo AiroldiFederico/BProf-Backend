eb264dde5315dda9cc41f4bdc32216d2fde84204
Federico Airoldi <airoldibusiness@gmail.com>
2023-07-21 10:57:30 +0200

    Style card profile

--- resources/views/admin/teachers/index.blade.php
+++ resources/views/admin/teachers/index.blade.php
@@ -31,60 +31,25 @@
   @if ($teacher != null)
 
 
-  <div class="col-6 m-auto mt-2">
+  <div class="m-auto mt-2 d-flex justify-content-center align-items-center h-100">
 
-    
-    <div class="card">
+    {{-- CARD PROFILO --}}
+    <div class="card-profile col-10 mt-5 px-4 py-4 row flex-column flex-lg-row">
 
-      <img src="{{asset('storage/' . $teacher->profile_picture)}}" class="card-img-top" alt="" width="90%">
 
-      <div class="card-body">
+      {{-- LEFT SIDE --}}
+      <div class="col-12 col-lg-4 text-center d-flex flex-column align-items-center h-100 ls-card">
 
-        {{-- Nome --}}
-        <h5 class="card-title mb-4">{{ $user->name }} {{$user->surname}}</h5>
-
-        {{-- Città --}}
-        <div class="d-flex card-subtitle mb-2">
-          <p class="me-1 fw-bold">Città:</p>
-          <p class="card-text"> {{$teacher->city}}</p>
+        <div class="img-card">
+          <img src="{{asset('storage/' . $teacher->profile_picture)}}" class="img-pp" alt="" width="90%">
         </div>
-
-        {{-- Indirizzo  --}}
-        <div class="d-flex card-subtitle mb-2">
-          <p class="me-1 fw-bold">Indirizzo:</p>
-          <p class="card-text"> {{$teacher->address}}</p>
-        </div>
-
-        {{-- CAP --}}
-        <div class="d-flex card-subtitle mb-2">
-          <p class="me-1 fw-bold">CAP:</p>
-          <p class="card-text"> {{$teacher->cap}}</p>
-        </div>
-
-        {{-- CV --}}
-        <div class="d-flex card-subtitle mb-2">
-          <p class="me-1 fw-bold">Curriculum:</p>
-          @if ($teacher->cv != null)
-            <a href="{{asset('storage/' . $teacher['cv'])}}" target="_blank">Guarda il CV</a>
-          @else
-            <p>Non hai ancora un Curriculum Vitae</p>
-          @endif
-        </div>
-
-        {{-- Descrizione --}}
-        <div class="d-flex card-subtitle mb-2">
-          <p class="me-1 fw-bold">Descrizione:</p>
-          <p class="card-text"> {{$teacher->description}}</p>
-        </div>
-
-        {{-- Prezzo --}}
-        <div class="d-flex card-subtitle mb-2">
-          <p class="me-1 fw-bold">Prezzo:</p>
-          @if ($teacher->price == 0)
-              Gratis
-          @else
-              {{$teacher->price}}
-          @endif
+        
+        <div class="d-flex mt-4">
+          {{-- Nome --}}
+          <h5 class="name-card">{{ $user->name }}</h5>
+          <span>&nbsp </span>
+          {{-- Cognome --}}
+          <h5 class="surname-card">{{ $user->surname }}</h5>
         </div>
 
         {{-- Materie --}}
@@ -99,65 +64,167 @@
             @endif
         </div>
 
-        {{-- Remote --}}
-        <div class="d-flex card-subtitle mb-2">
-          <p class="me-1 fw-bold">Remoto:</p>
-          @if ($teacher->remote != 0)
-              Si
-          @else
-              No
-          @endif
-        </div>
+      </div>
 
-        {{-- Cell --}}
-        <div class="d-flex card-subtitle mb-2">
-          <p class="me-1 fw-bold">Numero di telefono:</p>
-          <p class="card-text"> {{$teacher->phone_number}}</p>
-        </div>
+      {{-- RIGHT SIDE --}}
+      <div class="col-12 col-lg-8 position-relative rs-card ">
 
-        {{-- Bottone modifica --}}
-        <a href="{{route('teacher.edit', $teacher->id)}}" class="btn bg-success-subtle border-success text-success">
-          <i class="fa-solid fa-pencil"></i>
-          Modifica il profilo
-        </a>
-        
-        {{-- Delete --}}
-        <button type="button" class="btn btn-danger bg-danger-subtle text-danger me-2" data-bs-toggle="modal" data-bs-target="#deletModal">
-          <i class="fa-regular fa-trash-can"></i>
-          Cancella
-        </button>
-        <div class="modal fade" id="deletModal" tabindex="-1" aria-labelledby="deletModal" aria-hidden="true">
-          <div class="modal-dialog">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">Sei sicuro di voler eliminare il profilo?</h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-              </div>
-              <div class="modal-body">
-                <div class="container d-flex text-center justify-content-center mt-3 mb-3">
-                  <i class="fa-solid fa-triangle-exclamation text-danger fs-3"></i>
-                </div>
-                Se continui con l'eliminazione del profilo non potrai più tornare indietro
-              </div>
-              <div class="modal-footer justify-content-start">
-                <form action="{{ route('teacher.destroy', $teacher->id) }}" method="POST">
-                  @csrf
-                  @method('DELETE')
-                  <button type="submit" class="btn btn-danger bg-danger-subtle text-danger me-2">
-                    <i class="fa-regular fa-trash-can"></i>
-                    Delete
-                  </button>
-                </form>
-                <button type="button" class="btn btn-secondary bg-secondary-subtle text-secondary me-2" data-bs-toggle="modal" data-bs-dismiss="modal">
-                  <i class="fa-solid fa-xmark"></i>
-                  Cancel
-                </button>
-              </div>
-            </div>
+        <div class="mb-2">
+          <p class="title-right-card mb-0">INFORMAZIONI</p>
+          <div class="line">
+
           </div>
         </div>
 
+        {{-- TOP - RIGHT SIDE --}}
+        <div class="top-right d-flex">
+          
+          <div class="d-flex flex-column me-5">
+
+            {{-- Città --}}
+            <div class="d-flex card-subtitle mb-2">
+              <p class="me-1 fw-bold">Città:</p>
+              <p class="card-text"> {{$teacher->city}}</p>
+            </div>
+
+            {{-- CAP --}}
+            <div class="d-flex card-subtitle mb-2">
+              <p class="me-1 fw-bold">CAP:</p>
+              <p class="card-text"> {{$teacher->cap}}</p>
+            </div>
+
+          </div>
+
+          <div class="d-flex flex-column">
+
+            {{-- Indirizzo  --}}
+            <div class="d-flex card-subtitle mb-2">
+              <p class="me-1 fw-bold">Indirizzo:</p>
+              <p class="card-text"> {{$teacher->address}}</p>
+            </div>
+
+            {{-- Cell --}}
+            <div class="d-flex card-subtitle mb-2">
+              <p class="me-1 fw-bold">Numero di telefono:</p>
+              <p class="card-text"> {{$teacher->phone_number}}</p>
+            </div>
+
+          </div>
+
+        </div>
+
+        <div class="mb-2 mt-4">
+          <p class="title-right-card mb-0">SERVIZIO</p>
+          <div class="line">
+
+          </div>
+        </div>
+
+        {{-- Descrizione --}}
+        <div class="d-flex card-subtitle mb-2">
+          <p class="me-1 fw-bold">Descrizione:</p>
+          <p class="card-text"> {{$teacher->description}}</p>
+        </div>
+
+        <div class="d-flex gap-4">
+
+          {{-- Prezzo --}}
+          <div class="d-flex card-subtitle mb-2">
+            <p class="me-1 fw-bold">Prezzo:</p>
+            @if ($teacher->price == 0)
+                Gratis
+            @else
+                {{$teacher->price}}
+            @endif
+          </div>
+
+          {{-- Remote --}}
+          <div class="d-flex card-subtitle mb-2">
+            <p class="me-1 fw-bold">Remoto:</p>
+            @if ($teacher->remote != 0)
+                Si
+            @else
+                No
+            @endif
+          </div>
+
+        </div>
+
+        {{-- CV --}}
+        <div class="d-flex card-subtitle mb-5">
+          <p class="me-1 fw-bold">Curriculum:</p>
+          @if ($teacher->cv != null)
+            <a href="{{asset('storage/' . $teacher['cv'])}}" target="_blank">Guarda il CV</a>
+          @else
+            <p>Non hai ancora un Curriculum Vitae</p>
+          @endif
+        </div>
+
+        {{-- Button --}}
+        <div class=" position-absolute bottom-0">
+  
+          {{-- Bottone modifica --}}
+          <a href="{{route('teacher.edit', $teacher->id)}}" class="btn bg-success-subtle border-success text-success">
+            <i class="fa-solid fa-pencil"></i>
+            Modifica il profilo
+          </a>
+          
+          {{-- Delete --}}
+          <button type="button" class="btn btn-danger bg-danger-subtle text-danger me-2" data-bs-toggle="modal" data-bs-target="#deletModal">
+            <i class="fa-regular fa-trash-can"></i>
+            Cancella
+          </button>
+  
+        </div>
       </div>
+
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      <div class="modal fade" id="deletModal" tabindex="-1" aria-labelledby="deletModal" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="exampleModalLabel">Sei sicuro di voler eliminare il profilo?</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <div class="container d-flex text-center justify-content-center mt-3 mb-3">
+                <i class="fa-solid fa-triangle-exclamation text-danger fs-3"></i>
+              </div>
+              Se continui con l'eliminazione del profilo non potrai più tornare indietro
+            </div>
+            <div class="modal-footer justify-content-start">
+              <form action="{{ route('teacher.destroy', $teacher->id) }}" method="POST">
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="btn btn-danger bg-danger-subtle text-danger me-2">
+                  <i class="fa-regular fa-trash-can"></i>
+                  Delete
+                </button>
+              </form>
+              <button type="button" class="btn btn-secondary bg-secondary-subtle text-secondary me-2" data-bs-toggle="modal" data-bs-dismiss="modal">
+                <i class="fa-solid fa-xmark"></i>
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
     </div>
   </div>
   @else
