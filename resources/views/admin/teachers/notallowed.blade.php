f76f0bf24a5be25587a8835260ca3d2791154cfc
gluke0 <milanesigianluca@gmail.com>
2023-07-21 08:24:58 +0200

    text notallowed

--- resources/views/admin/teachers/notallowed.blade.php
+++ resources/views/admin/teachers/notallowed.blade.php
@@ -5,10 +5,13 @@
 <div class="content">
     <div class="container">
 
-        <div class="bord"> 
+        <div class="bord d-flex justify-content-center flex-wrap"> 
+            <h1 class="notallowed">
+                Non puoi modificare il profilo di un altro professore!
+            </h1>
             <button onclick="history.back()" class="btn bg-danger-subtle border-danger text-danger">
                 <i class="fa-solid fa-pencil"></i>
-                Furbino torna alla modifica del tuo profilo
+                Torna alla modifica del tuo profilo
             </button>
        </div>
 
@@ -17,6 +20,11 @@
 
 <style>
     .bord{
-        margin-top: 10rem
+        margin-top: 15rem;
+    }
+
+    .notallowed{
+        color: red;
+        font-size: 3.5rem
     }
 </style>
