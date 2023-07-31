82ad930c7d40ca19855fcdb4de30d98b7ec24f13
gluke0 <milanesigianluca@gmail.com>
2023-07-31 16:06:22 +0200

    not allowed

--- resources/views/admin/teachers/notallowed.blade.php
+++ resources/views/admin/teachers/notallowed.blade.php
@@ -8,11 +8,10 @@
     <div class="container">
 
         <div class="bord d-flex justify-content-center flex-wrap"> 
-            <h1 class="notallowed">
-                Non puoi modificare il profilo di un altro professore!
+            <h1 class="notallowed text-center">
+                Non è consentito modificare il profilo di un altro professore!
             </h1>
-            <button onclick="history.back()" class="btn bg-danger-subtle border-danger text-danger">
-                <i class="fa-solid fa-pencil"></i>
+            <button onclick="history.back()" class="btn_reg_el">
                 Torna alla modifica del tuo profilo
             </button>
        </div>
@@ -29,4 +28,18 @@
         color: red;
         font-size: 3.5rem
     }
+
+    .btn_reg_el{
+  border: 2px solid rgb(225, 27, 27);
+  outline: none;
+  padding: 7px 21px;
+  border-radius: 32px;
+  background: transparent;
+  backdrop-filter: blur(10px);
+  display: inline;
+  align-items: center;
+  cursor: pointer;
+  transition: all 200ms ease;
+  background-color: rgb(225, 27, 27);
+}
 </style>
